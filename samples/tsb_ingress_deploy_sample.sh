b97593671a7fc53dba0bb58221c4734765703506
Sergey Marunich <marunich.s@gmail.com>
2021-12-20 05:29:38 -0500

    Update tsb_ingress_deploy_sample.sh

--- samples/tsb_ingress_deploy_sample.sh
+++ samples/tsb_ingress_deploy_sample.sh
@@ -1,11 +1,22 @@
+export REGISTRY="mstsbacrx9pslvvlqec0jpg3.azurecr.io"
+export FOLDER="."
+export BASE_FQDN="example.com"
+export TSB_FQDN_PREFIX="tsb"
+export TSB_FQDN="ms-station.cx.tetrate.info"
+export TSB_GATEWAY="tsb-gateway"
+export APP_NAME="bookinfo"
+export APP_NAMESPACE="bookinfo"
 
+./tctl140 config clusters set default --tls-insecure --bridge-address $(kubectl get svc -n tsb envoy --output jsonpath='{.status.loadBalancer.ingress[0].ip}'):8443
+./tctl140 config profiles set-current default
+./tctl140 login --org tetrate --username admin --password Tetrate123 --tenant tetrate
 
-cat >"${FOLDER}/ingress-gateway.yaml" <<EOF
+cat >"${FOLDER}/install-ingress-gateway.yaml" <<EOF
 apiVersion: install.tetrate.io/v1alpha1
 kind: IngressGateway
 metadata:
-  name: tsb-gateway-bookinfo
-  namespace: bookinfo
+  name: ${TSB_GATEWAY}
+  namespace: ${APP_NAMESPACE}
 spec:
   kubeSpec:
     service:
@@ -14,4 +25,27 @@ spec:
         service.beta.kubernetes.io/azure-load-balancer-internal: "false"
 EOF
 
+k apply -f ${FOLDER}/install-ingress-gateway.yaml
 
+cat >"${FOLDER}/bookinfo-ingress-gateway.yaml" <<EOF
+apiVersion: gateway.tsb.tetrate.io/v2
+kind: IngressGateway
+Metadata:
+  organization: tetrate
+  tenant: tetrate
+  name: bookinfo-gw-ingress
+  group: bookinfo-gw
+  workspace: bookinfo-ws
+spec:
+  workloadSelector:
+    namespace: ${APP_NAMESPACE}
+    labels:
+      app: ${TSB_GATEWAY}
+  http:
+    - name: bookinfo
+      hostname: "bookinfo.tetrate.com"
+      routing:
+        rules:
+          - route:
+              host: "bookinfo/productpage.bookinfo.svc.cluster.local"
+EOF
