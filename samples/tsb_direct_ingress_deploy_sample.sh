a62506afe7a2fe793e3af8d25332e7cc68c6c82f
Sergey Marunich <marunich.s@gmail.com>
2021-12-21 06:32:07 -0500

    Update tsb_direct_ingress_deploy_sample.sh

--- samples/tsb_direct_ingress_deploy_sample.sh
+++ samples/tsb_direct_ingress_deploy_sample.sh
@@ -22,3 +22,43 @@ spec:
     hosts:
     - *
 EOF
+
+cat >"${FOLDER}/dr-sample.yaml" <<EOF
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  annotations:
+    tsb.tetrate.io/organization: {{ORGANIZATION}}
+    tsb.tetrate.io/tenant: {{TENANT}}
+    tsb.tetrate.io/workspace: {{WORK_SPACE}}
+    tsb.tetrate.io/gatewayGroup: {{GATEWAY_GROUP}}
+  name: dr-{{APP_NAME}}
+spec:
+  host: {{APP_NAME}}
+  subsets:
+  - name: v1
+    labels:
+      version: v1
+EOF
+
+cat >"${FOLDER}/vs-sample.yaml" <<EOF
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  annotations:
+    tsb.tetrate.io/organization: {{ORGANIZATION}}
+    tsb.tetrate.io/tenant: {{TENANT}}
+    tsb.tetrate.io/workspace: {{WORK_SPACE}}
+    tsb.tetrate.io/gatewayGroup: {{GATEWAY_GROUP}}
+  name: vs-{{APP_NAME}}
+spec:
+  http:
+  - route:
+    - destination:
+        host: {{APP_NAME}}
+        subset: v1
+EOF
+
+
