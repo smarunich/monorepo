80b36e7651217e7678aee253d2e3650255120e9c
Sergey Marunich <marunich.s@gmail.com>
2021-12-27 04:53:06 -0500

    Update tsb_tier1_bootstrap.sh

--- samples/tsb_tier1_bootstrap.sh
+++ samples/tsb_tier1_bootstrap.sh
@@ -1,7 +1,27 @@
 export REGISTRY="mstsbacrx9pslvvlqec0jpg3.azurecr.io"
 export FOLDER="."
 export TSB_FQDN="ms-station.cx.tetrate.info"
-   
+
+tctl config clusters set default --tls-insecure --bridge-address $(kubectl get svc -n tsb envoy --output jsonpath='{.status.loadBalancer.ingress[0].ip}'):8443
+tctl config profiles set-current default
+tctl login --org tetrate --username admin --password Tetrate123 --tenant tetrate
+
+cat >"${FOLDER}/install-tier1-gateway.yaml" <<EOF
+apiVersion: install.tetrate.io/v1alpha1
+kind: Tier1Gateway
+metadata:
+  name: tier1-vm
+  namespace: tier1
+spec:
+  kubeSpec:
+    service:
+      type: LoadBalancer
+      annotations:
+        service.beta.kubernetes.io/azure-load-balancer-internal: "false"
+EOF
+
+kubectl apply -f "${FOLDER}/install-tier1-gateway.yaml"
+
 cat >"${FOLDER}/tier1gateway.yaml" <<EOF
 ---
 apiVersion: gateway.tsb.tetrate.io/v2
@@ -18,3 +38,5 @@ spec:
     labels:
       app: tier1-vm
 EOF
+
+tctl apply -f "${FOLDER}/tier1gateway.yaml" 
