63b8af51a527ad26cbcc6fcc62aa1212291e4719
Sergey Marunich <marunich.s@gmail.com>
2022-01-10 05:24:43 -0500

    Update create_team_and_user.sh

--- samples/create_team_and_user.sh
+++ samples/create_team_and_user.sh
@@ -5,27 +5,64 @@ cat >"${FOLDER}/teams_and_users.yaml" <<EOF
 apiVersion: api.tsb.tetrate.io/v2
 kind: Team
 metadata:
-  displayName: Payroll
-  name: Payroll
-  organization: cx-nist-demo-org
+  displayName: appdevops-team-a
+  name: appdevops-team-a
+  organization: tetrate
 spec:
-  displayName: Payroll
-  fqn: organizations/cx-nist-demo-org/teams/Payroll
+  displayName: appdevops-team-a
   members:
-  - organizations/cx-nist-demo-org/users/BotA
+  - organizations/tetrate/users/BotA
 ---
 apiVersion: api.tsb.tetrate.io/v2
 kind: User
 metadata:
   displayName: cn=Andi Bot,ou=Human Resources,dc=tetrate,dc=io
   name: BotA
-  organization: cx-nist-demo-org
+  organization: tetrate
 spec:
   displayName: cn=Andi Bot,ou=Human Resources,dc=tetrate,dc=io
   email: BotA@ns-mail7.com
-  fqn: organizations/cx-nist-demo-org/users/BotA
+  fqn: organizations/tetrate/users/BotA
   loginName: BotA
   sourceType: LDAP
 EOF
 
 tctl apply -f "${FOLDER}/teams_and_users.yaml"
+
+# Define role https://docs.tetrate.io/service-bridge/1.4.x/en-us/refs/tsb/rbac/v2/role#role
+cat >"${FOLDER}/sample_role.yaml" <<EOF
+---
+apiVersion: rbac.tsb.tetrate.io/v2
+kind: Role
+metadata:
+  description: appdevops
+  displayName: appdevops
+  name: appdevops
+spec:
+  description: appdevops
+  displayName: appdevops
+  fqn: rbac/appdevops
+  rules:
+  - permissions:
+    - READ
+    - WRITE
+    - CREATE
+    - DELETE
+EOF
+
+tctl apply -f "${FOLDER}/sample_role.yaml"
+
+# Bind the role https://docs.tetrate.io/service-bridge/1.4.x/en-us/refs/tsb/rbac/v2/tenant_access_bindings
+cat >"${FOLDER}/sample_wab.yaml" <<EOF
+apiVersion: rbac.tsb.tetrate.io/v2
+kind: WorkspaceAccessBindings
+metadata:
+  organization: myorg
+  tenant: mycompany
+  workspace: w1
+spec:
+  allow:
+  - role: rbac/appdevops
+    subjects:
+    - team: organization/tetrate/teams/appdevops-team-a
+EOF
