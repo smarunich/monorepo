e94a905684a8e668ce813059936383872661e16a
Sergey Marunich <marunich.s@gmail.com>
2022-01-10 05:09:48 -0500

    Create create_team_and_user.sh

---
+++ samples/create_team_and_user.sh
@@ -0,0 +1,30 @@
+---
+apiVersion: api.tsb.tetrate.io/v2
+kind: Team
+metadata:
+  displayName: Payroll
+  name: Payroll
+  organization: cx-nist-demo-org
+  resourceVersion: '"cMHEuQs6i6Y="'
+spec:
+  displayName: Payroll
+  etag: '"cMHEuQs6i6Y="'
+  fqn: organizations/cx-nist-demo-org/teams/Payroll
+  members:
+  - organizations/cx-nist-demo-org/users/NallY
+  - organizations/cx-nist-demo-org/users/MadaniG
+---
+apiVersion: api.tsb.tetrate.io/v2
+kind: User
+metadata:
+  displayName: cn=Andi Bot,ou=Human Resources,dc=tetrate,dc=io
+  name: BotA
+  organization: cx-nist-demo-org
+  resourceVersion: '"MMli44tAUj0="'
+spec:
+  displayName: cn=Andi Bot,ou=Human Resources,dc=tetrate,dc=io
+  email: BotA@ns-mail7.com
+  etag: '"MMli44tAUj0="'
+  fqn: organizations/cx-nist-demo-org/users/BotA
+  loginName: BotA
+  sourceType: LDAP
