498d3e5bf9614b3ea753f59fcdd2701f6d3b2a2d
Sergey Marunich <marunich.s@gmail.com>
2022-01-10 05:12:10 -0500

    Update create_team_and_user.sh

--- samples/create_team_and_user.sh
+++ samples/create_team_and_user.sh
@@ -1,3 +1,6 @@
+export FOLDER="."
+
+cat >"${FOLDER}/teams_and_users.yaml" <<EOF
 ---
 apiVersion: api.tsb.tetrate.io/v2
 kind: Team
@@ -5,14 +8,11 @@ metadata:
   displayName: Payroll
   name: Payroll
   organization: cx-nist-demo-org
-  resourceVersion: '"cMHEuQs6i6Y="'
 spec:
   displayName: Payroll
-  etag: '"cMHEuQs6i6Y="'
   fqn: organizations/cx-nist-demo-org/teams/Payroll
   members:
-  - organizations/cx-nist-demo-org/users/NallY
-  - organizations/cx-nist-demo-org/users/MadaniG
+  - organizations/cx-nist-demo-org/users/BotA
 ---
 apiVersion: api.tsb.tetrate.io/v2
 kind: User
@@ -20,11 +20,12 @@ metadata:
   displayName: cn=Andi Bot,ou=Human Resources,dc=tetrate,dc=io
   name: BotA
   organization: cx-nist-demo-org
-  resourceVersion: '"MMli44tAUj0="'
 spec:
   displayName: cn=Andi Bot,ou=Human Resources,dc=tetrate,dc=io
   email: BotA@ns-mail7.com
-  etag: '"MMli44tAUj0="'
   fqn: organizations/cx-nist-demo-org/users/BotA
   loginName: BotA
   sourceType: LDAP
+EOF
+
+tctl apply -f "${FOLDER}/teams_and_users.yaml"
