0756dc7916d184a547ad6cb2eadba1b825dd1731
Sergey Marunich <marunich.s@gmail.com>
2022-01-12 05:58:59 -0500

    productpage demo

---
+++ samples/productpage_patch.sh
@@ -0,0 +1,15 @@
+export FOLDER='.'
+
+cat >"${FOLDER}/productpage_patch.yaml" <<EOF
+spec:
+  template:
+    spec:
+      containers:
+      - name: productpage
+        image: docker.io/chirauki/productpage
+        env:
+        - name: DETAILS_HOSTNAME
+          value: details.tetrate.com
+EOF
+
+kubectl -n $TIER2_NS patch deployments.apps/productpage-v1 --patch "$(cat "$FOLDER"/templates/productpage_patch.yaml)" >/dev/null 2>/dev/null
