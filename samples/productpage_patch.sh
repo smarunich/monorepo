d28e74b0a716a3e262b4175e74f3320fa0d06f01
Sergey Marunich <marunich.s@gmail.com>
2022-01-14 03:23:20 -0500

    Update productpage_patch.sh

--- samples/productpage_patch.sh
+++ samples/productpage_patch.sh
@@ -10,6 +10,9 @@ spec:
         env:
         - name: DETAILS_HOSTNAME
           value: details.tetrate.com
+        - name: DETAILS_SERVICE_PORT
+          value: 80
+      
 EOF
 
 kubectl -n $TIER2_NS patch deployments.apps/productpage-v1 --patch "$(cat "$FOLDER"/templates/productpage_patch.yaml)" >/dev/null 2>/dev/null
