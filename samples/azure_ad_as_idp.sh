c57990353d807290c1968e817931f76f95b6d6f4
Sergey Marunich <marunich.s@gmail.com>
2022-01-17 06:07:32 -0500

    Update azure_ad_as_idp.sh

--- samples/azure_ad_as_idp.sh
+++ samples/azure_ad_as_idp.sh
@@ -1,16 +1,35 @@
-az ad app create 
-  --display-name TSB \
-  --reply-urls "https://<TSB_IP>:8443/iam/v2/oidc/callback" \
+export FOLDER='.'
+export TSB_FQDN="ms-station.cx.tetrate.info"
+
+cat >"${FOLDER}/requireResourceManifest.json" <<EOF
+[
+  {
+    "resourceAppId": "00000002-0000-0000-c000-000000000000",
+    "resourceAccess": [
+      {
+        "id": "5778995a-e1bf-45b8-affa-663a9f3f4d04",
+        "type": "Role"
+      }
+    ]
+  }
+]
+EOF
+
+az ad app create \
+  --display-name TSB-${TSB_FQDN} \
+  --reply-urls "https://${TSB_FQDN}:8443/iam/v2/oidc/callback" \
   --oauth2-allow-implicit-flow true \
   --required-resource-accesses @requireResourceManifest.json
   
-az ad app  credential reset  --id <appID>  --append
+export TSB_APP_ID=`az ad app list --display-name=TSB-${TSB_FQDN} | jq -r '.[].appId'`
+  
+az ad app  credential reset  --id $TSB_APP_ID  --append
 
-az ad sp create --id <appID>
+az ad sp create --id  $TSB_APP_ID
 
-az ad app permission admin-consent --id  <appID>
+az ad app permission admin-consent --id  $TSB_APP_ID
 
-az ad app permission grant --id <appID> --api 00000002-0000-0000-c000-000000000000
+az ad app permission grant --id $TSB_APP_ID --api 00000002-0000-0000-c000-000000000000
 
 # In order to use the TSB token, a user record must be created in TSB with a user ID matching the subject claim from the AzureAD Service Principal, which is also the same as the Client ID from the AzureAD App Registration screen. In addition, the AzureAD App Registration must also set accessTokenAcceptedVersion=2 in the App Manifest. This enables TSB to perform token checks required for the token exchange grant.
 
