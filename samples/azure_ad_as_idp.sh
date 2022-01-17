cbfb98d3d531610c372c3ab8b087c7ea8fd83c58
Sergey Marunich <marunich.s@gmail.com>
2022-01-17 07:16:44 -0500

    Update azure_ad_as_idp.sh

--- samples/azure_ad_as_idp.sh
+++ samples/azure_ad_as_idp.sh
@@ -1,13 +1,14 @@
 export FOLDER='.'
 export TSB_FQDN="ms-station.cx.tetrate.info"
+export AZURE_TENANT_ID="1076231c-4971-42f2-8c33-aa8680e81ea8"                 
 
 cat >"${FOLDER}/requireResourceManifest.json" <<EOF
 [
   {
-    "resourceAppId": "00000002-0000-0000-c000-000000000000",
+    "resourceAppId": "00000003-0000-0000-c000-000000000000",
     "resourceAccess": [
       {
-        "id": "5778995a-e1bf-45b8-affa-663a9f3f4d04",
+        "id": "7ab1d382-f21e-4acd-a863-ba3e13f7da61",
         "type": "Role"
       }
     ]
@@ -29,46 +30,48 @@ az ad sp create --id  $TSB_APP_ID
 
 az ad app permission admin-consent --id  $TSB_APP_ID
 
-az ad app permission grant --id $TSB_APP_ID --api 00000002-0000-0000-c000-000000000000
+az ad app permission grant --id $TSB_APP_ID --api 00000003-0000-0000-c000-000000000000
 
 # In order to use the TSB token, a user record must be created in TSB with a user ID matching the subject claim from the AzureAD Service Principal, which is also the same as the Client ID from the AzureAD App Registration screen. In addition, the AzureAD App Registration must also set accessTokenAcceptedVersion=2 in the App Manifest. This enables TSB to perform token checks required for the token exchange grant.
 
 #tctl install manifest management-plane-secrets --allow-defaults --tsb-admin-password <TSB_Admin_Pass>   \
     --oidc-client-secret=<Client_Secret> \
     --teamsync-azure-client-secret=<Client_Secret> > secret.yaml
- 
+
+
+kubectl -n tsb create secret generic iam-oidc-client-secret --from-literal=client-secret=''
+kubectl -n tsb create secret generic azure-credentials --from-literal=client-secret=''
+
 #kubectl apply -f secret.yaml
 
-tctl edit managementplane managementplane -n tsb
+kubectl edit managementplane managementplane -n tsb
 
-apiVersion: install.tetrate.io/v1alpha1
-kind: ManagementPlane
-metadata:
-  name: managementplane
-  namespace: tsb
+
+cat >"${FOLDER}/managementplane_oidc_patch.yaml" <<EOF
 spec:
-  ( … )
   identityProvider:
     oidc:
-      clientId: <the application client id>
+      clientId: ${TSB_APP_ID}
       providerConfig:
         dynamic:
-          configurationUri: <the application configuration uri>
-      redirectUri: https://<tsb-address>:8443/iam/v2/oidc/callback
+          configurationUri: https://login.microsoftonline.com/$AZURE_TENANT_ID/v2.0/.well-known/openid-configuration
+      redirectUri: https://$TSB_FQDN:8443/iam/v2/oidc/callback
       scopes:
       - email
       - profile
       - offline_access
       offlineAccessConfig:
         deviceCodeAuth:
-          clientId: <the application client id>
-            offlineAccessConfig:
+          clientId: ${TSB_APP_ID}
         tokenExchange:
-          clientId: <the application client id>
+          clientId: ${TSB_APP_ID}
     sync:
       azure:
-        clientId: <the application client id>
-        tenantId: <the application tenant id>
+        clientId: ${TSB_APP_ID}
+        tenantId: ${AZURE_TENANT_ID}
+EOF
+  
+kubectl -n tsb patch managementplane/managementplane --patch "$(cat "$FOLDER"/managementplane_oidc_patch.yaml)" --type=merge
         
 # curl -X POST  \
   -H 'Content-Type: application/x-www-form-urlencoded' \
