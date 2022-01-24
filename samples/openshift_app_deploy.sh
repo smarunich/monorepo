955d18f0c848d5dc7f0c5f9e1c5ca09d895fe5d9
Sergey Marunich <marunich.s@gmail.com>
2022-01-24 05:27:03 -0500

    Update openshift_app_deploy.sh

--- samples/openshift_app_deploy.sh
+++ samples/openshift_app_deploy.sh
@@ -12,7 +12,7 @@ EOF
 oc create namespace $APP_NS
 oc label namespace $APP_NS istio-injection=enabled
 
-oc -n $APP_NS create -f $FOLDER/templates/oc_networkattach.yaml
+oc -n $APP_NS create -f $FOLDER/oc_networkattach.yaml
 oc adm policy add-scc-to-group anyuid system:serviceaccounts:$APP_NS
 
 oc rollout restart deployment -n $APP_NS
