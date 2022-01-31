8db0aa828244c9e4969252a19bf4b50bc87e649b
Sergey Marunich <marunich.s@gmail.com>
2022-01-31 05:31:31 -0500

    wasm-local-filter

---
+++ wasm/wasm_local.sh
@@ -0,0 +1,23 @@
+export NAMESPACE="bookinfo"
+export FOLDER="."
+
+wasm_image=1.9.0.wasm
+kubectl -n $NAMESPACE create configmap wasm-header --from-file=$wasm_image
+
+
+
+
+cat >"${FOLDER}/wasm-patch-annotations.yaml" <<EOF
+```
+spec:
+  template:
+    metadata:
+      annotations:
+        sidecar.istio.io/userVolume: '[{"name":"wasmfilters-dir","configMap": {"name":"wasm-header"}}]'
+        sidecar.istio.io/userVolumeMount: '[{"mountPath":"/var/local/lib/wasm-filters","name":"wasmfilters-dir"}]'
+```
+
+patch_annotations=$(cat patch-annotations.yaml)
+
+kubectl -n $NAMESPACE patch deployment ratings-v1 -p "$patch_annotations"
+
