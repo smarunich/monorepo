dc4495c45630ce5becad2307edb3144e5b7e5b72
Sergey Marunich <marunich.s@gmail.com>
2022-02-02 05:26:25 -0500

    Update wasm_local.sh

--- wasm/wasm_local.sh
+++ wasm/wasm_local.sh
@@ -18,6 +18,23 @@ patch_annotations=$(cat patch-annotations.yaml)
 kubectl -n $NAMESPACE patch deployment ratings-v1 -p "$patch_annotations"
 
 #kubectl -n $NAMESPACE patch deployment  frontpage-v1 -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"wasm_header\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
-kubectl -n $NAMESPACE exec -it deployment/frontpage-v1 -c istio-proxy -- ls /var/local/lib/wasm-filters/
 
+
+# multiple filters using common configmap
+
+cat >"${FOLDER}/wasm-patch-annotations.yaml" <<EOF
+```
+spec:
+  template:
+    metadata:
+      annotations:
+        sidecar.istio.io/userVolume: '[{"name":"wasmfilters-dir","configMap": {"name":"wasm_plugins"}}]'
+        sidecar.istio.io/userVolumeMount: '[{"mountPath":"/var/local/lib/wasm-filters","name":"wasmfilters-dir"}]'
+```
+
+patch_annotations=$(cat patch-annotations.yaml)
+kubectl -n $NAMESPACE patch deployment productpage-v1 -p "$patch_annotations"
+kubectl -n $NAMESPACE patch deployment reviews-v1 -p "$patch_annotations"
+kubectl -n $NAMESPACE patch deployment reviews-v2 -p "$patch_annotations"
+kubectl -n $NAMESPACE patch deployment reviews-v3 -p "$patch_annotations"
 
