c852e7f981ba5a1abe7a0fb6df2f2933c4fa2003
Sergey Marunich <marunich.s@gmail.com>
2022-06-24 15:01:27 -0400

    secrets order

--- helm/helm_external_secrets_model/deploy_mp_using_helm.sh
+++ helm/helm_external_secrets_model/deploy_mp_using_helm.sh
@@ -1,2 +1,3 @@
-helm install tsb-managementplane --debug --create-namespace -n tsb -f mp-helm-values-no-secrets-provided.yaml tetrate-tsb-helm/managementplane --version 1.5.0-EA1 --devel
+kubectl create ns tsb
 kubectl apply -f mp-secrets.yaml
+helm install tsb-managementplane --debug -n tsb -f mp-helm-values-no-secrets-provided.yaml tetrate-tsb-helm/managementplane --version 1.5.0-EA1 --devel
