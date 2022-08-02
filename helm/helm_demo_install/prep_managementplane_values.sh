802dc7a1731780ce0db43868ef3888f0c03df668
Sergey Marunich <marunich.s@gmail.com>
2022-08-02 15:00:36 -0400

    Update prep_managementplane_values.sh

--- helm/helm_demo_install/prep_managementplane_values.sh
+++ helm/helm_demo_install/prep_managementplane_values.sh
@@ -10,16 +10,16 @@ secrets:
   tsb:
     adminPassword: Tetrate123
     cert: | 
-      $(for line in $(cat tsb_certs.crt); do echo "   $line"; done;
+      $(for line in $(cat tsb_certs.crt); do echo "   $line"; done;)
     key: 
-      $(for line in $(cat tsb_certs.key); do echo "   $line"; done;
+      $(for line in $(cat tsb_certs.key); do echo "   $line"; done;)
   xcp:
     autoGenerateCerts: false
     central:
       cert: |
-        $(for line in $(cat xcp-central-cert.crt); do echo "   $line"; done;
+        $(for line in $(cat xcp-central-cert.crt); do echo "   $line"; done;)
       key: | 
-        $(for line in $(cat xcp-central-cert.key); do echo "   $line"; done;
+        $(for line in $(cat xcp-central-cert.key); do echo "   $line"; done;)
     rootca: |
       $(for line in $(cat ca.crt); do echo "   $line"; done;)
 spec:
