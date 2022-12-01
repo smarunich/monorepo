76b90c04151b80e58614e58c16cf11885acfda57
Sergey Marunich <marunich.s@gmail.com>
2022-12-01 11:46:49 -0500

    more of typos

---
+++ random-connections/v2-passim-connections/apim-test.sh
@@ -0,0 +1,1 @@
+curl -Ik https://external-api-httpbin.gcp.cx.tetrate.info -H"Host: external-api-httpbin.gcp.cx.tetrate.info" --resolve external-api-httpbin.gcp.cx.tetrate.info:443:34.74.90.140 -X GET
