da6c6083bd1b9973a00498df543809da19112246
Sergey Marunich <marunich.s@gmail.com>
2022-12-08 14:09:23 -0500

    v3-casual

---
+++ random-connections/v3-casual-connections/apim-test.sh
@@ -0,0 +1,1 @@
+curl -Ik https://external-api-httpbin.gcp.cx.tetrate.info -H"Host: external-api-httpbin.gcp.cx.tetrate.info" --resolve external-api-httpbin.gcp.cx.tetrate.info:443:20.83.131.18 -X GET
