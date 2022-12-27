8da94a09fd6837703cad89acedd0d44a22183b20
Sergey Marunich <marunich.s@gmail.com>
2022-12-27 11:13:51 -0500

    more tests around

--- random-connections/v3-casual-connections/apim-test.sh
+++ random-connections/v3-casual-connections/apim-test.sh
@@ -1,1 +1,4 @@
-curl -Ik https://external-api-httpbin.gcp.cx.tetrate.info -H"Host: external-api-httpbin.gcp.cx.tetrate.info" --resolve external-api-httpbin.gcp.cx.tetrate.info:443:20.83.131.18 -X GET
+echo "consumer-service-httpbin.gcp.cx.tetrate.info: `curl -Isk http://20.83.131.18 -H"Host: consumer-service-httpbin.gcp.cx.tetrate.info"  -X GET | grep HTTP`"
+echo "external-api-httpbin.gcp.cx.tetrate.info: `curl -Isk http://20.83.131.18 -H"Host: external-api-httpbin.gcp.cx.tetrate.info"  -X GET | grep HTTP`"
+echo "rogue-external-api-httpbin.gcp.cx.tetrate.info: `curl -Isk http://20.83.131.18 -H"Host: rogue-external-api-httpbin.gcp.cx.tetrate.info"  -X GET | grep HTTP`"
+echo "external-api-httpbin.gcp.cx.tetrate.info: `curl -Isk https://external-api-httpbin.gcp.cx.tetrate.info -H"Host: external-api-httpbin.gcp.cx.tetrate.info" --resolve external-api-httpbin.gcp.cx.tetrate.info:443:20.83.131.18 -X GET | grep HTTP`"
